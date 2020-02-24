ef6c7818159313025c0db0eaef1c19bc52fbfdcb
Opryatnov Dmitriy <apratnau_ds@st.by>
2020-02-24 11:09:19 +0300

    Create project

---
+++ CurrencyNBRB/CurrencyNBRB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrencyNBRB
+//
+//  Created by Dimon on 2/24/20.
+//  Copyright © 2020 Dimon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
